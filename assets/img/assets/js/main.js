4bbb84e6a9f3d7cb92e63391287679901ae1cdaf
mahmudur rahman <mrhridoy397@gmail.com>
2023-04-02 12:21:20 +0600

    ready for remot origin

---
+++ assets/img/assets/js/main.js
@@ -0,0 +1,35 @@
+$('.events-carousel').owlCarousel({
+    center:true,
+    loop:true,
+    // margin:10,
+    nav:false,
+    dots:true,
+    dotsEach:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// });
